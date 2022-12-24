52323e8d9a1caf0750ae797b9389c4a14bba0fcf
SeanQuinn <Seanquinn917@gmail.com>
2022-12-24 17:04:14 -0500

    commit tree 138ad310733c694fd1b4b6cf0ddaf5b0ed4a51a2

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2
+const num2 = 31
+let multiply;
+multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random()+1) ;
+
+const num3 = 16
+const num4 = 6
+
+let mod;
+mod = num3 % num4;
+
+let max = 20;
+
